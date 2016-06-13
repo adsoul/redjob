1a150b12d18643c5309e8d9626995950c60ec59d
Markus Heiden <markus.heiden@s24.com>
2016-06-13 14:11:11 +0200

    Add test for priority enqueue

--- src/integration-test/java/com/s24/redjob/queue/QueueDaoImplIT.java
+++ src/integration-test/java/com/s24/redjob/queue/QueueDaoImplIT.java
@@ -33,17 +33,47 @@ public class QueueDaoImplIT {
    }
 
    @Test
-   public void enqueue() {
+   public void enqueue_normal() {
       String queue = "test";
 
-      long id = dao.enqueue(queue, new TestJob("value"), false).getId();
-      assertTrue(id > 0);
+      TestJob job1 = new TestJob();
+      long id1 = dao.enqueue(queue, job1, false).getId();
+      assertTrue(id1 > 0);
+      TestJob job2 = new TestJob();
+      long id2 = dao.enqueue(queue, job2, false).getId();
+      assertTrue(id2 > 0);
 
-      Execution execution = dao.pop(queue, "worker");
-      assertNotNull(execution);
-      assertEquals(id, execution.getId());
-      TestJob job = (TestJob) execution.getJob();
-      assertNotNull(job);
-      assertEquals("value", job.getValue());
+      Execution execution1 = dao.pop(queue, "worker");
+      assertNotNull(execution1);
+      assertEquals(id1, execution1.getId());
+      assertEquals(job1, execution1.getJob());
+
+      Execution execution2 = dao.pop(queue, "worker");
+      assertNotNull(execution2);
+      assertEquals(id2, execution2.getId());
+      assertEquals(job2, execution2.getJob());
+   }
+
+   @Test
+   public void enqueue_priority() {
+      String queue = "test";
+
+      TestJob jobNormal = new TestJob();
+      long idNormal = dao.enqueue(queue, jobNormal, false).getId();
+      assertTrue(idNormal > 0);
+      TestJob jobPriority = new TestJob();
+      long idPriority = dao.enqueue(queue, jobPriority, true).getId();
+      assertTrue(idPriority > 0);
+
+      // Check that due to the priority flag set to true, the priority job gets executed first, even if scheduled later.
+      Execution execution1 = dao.pop(queue, "worker");
+      assertNotNull(execution1);
+      assertEquals(idPriority, execution1.getId());
+      assertEquals(jobPriority, execution1.getJob());
+
+      Execution execution2 = dao.pop(queue, "worker");
+      assertNotNull(execution2);
+      assertEquals(idNormal, execution2.getId());
+      assertEquals(jobNormal, execution2.getJob());
    }
 }
