d5c22fe1d8452064a68dfcd9638dae5db92b808d
Markus Heiden <markus@markusheiden.de>
2016-06-23 22:04:27 +0200

    Make return value more flexible

--- src/main/java/com/s24/redjob/worker/WrappingRunnable.java
+++ src/main/java/com/s24/redjob/worker/WrappingRunnable.java
@@ -22,7 +22,7 @@ public abstract class WrappingRunnable implements Runnable {
    /**
     * Job runner.
     */
-   public Object unwrap() {
-      return runner;
+   public <R> R unwrap() {
+      return (R) runner;
    }
 }
