6ee9897a0b5ceab8a4ce7d8262f9487e32d3db84
Markus Heiden <markus.heiden@s24.com>
2018-02-23 23:58:43 +0100

    Introduce qualifier for connection factory to use for RedJob

--- src/main/java/com/s24/redjob/RedJobRedisConnectionFactory.java
+++ src/main/java/com/s24/redjob/RedJobRedisConnectionFactory.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
 /**
  * Qualifier for the {@link RedisConnectionFactory} to use for RedJob.
  */
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Qualifier
